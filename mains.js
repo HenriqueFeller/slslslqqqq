9b88ab2ad48ffd8a7cd32841d33260ae44abe86c
HenriqueFeller <henrique.feller@escola.pr.gov.br>
2024-09-25 12:23:48 -0300

    a

--- mains.js
+++ mains.js
@@ -12,5 +12,5 @@ let mediaa = function (n1, n2 , n3) {
     return (n1 + n2 + n3) / 2;
  }
  console.log(media(6, 4, 2));
- let numero = prompt ( (qual a média?) )
+ let numero=prompt ((qual a média?))
  document.write ( 6 )
