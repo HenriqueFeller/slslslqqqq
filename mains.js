9a904f7896069cdfc56cdddea542d553c98c88ec
HenriqueFeller <henrique.feller@escola.pr.gov.br>
2024-09-25 12:21:48 -0300

    a

--- mains.js
+++ mains.js
@@ -7,10 +7,10 @@ function Quadrado (num1) {
     return num1*num1
 }
 console.log(Quadrado(2));
-let media = function (n1, n2 , n3) {
+let mediaa = function (n1, n2 , n3) {
 
     return (n1 + n2 + n3) / 2;
  }
- console.log(mediaa(6, 4, 2));
- let nome = prompt ( (qual a media) ?)
+ console.log(media(6, 4, 2));
+ let Quadrado = prompt ( qual a média ?)
  document.write ( 6 )
