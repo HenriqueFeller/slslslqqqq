bd5375dbf1a969cb5ac7a86da645ed30a2de1cf1
HenriqueFeller <henrique.feller@escola.pr.gov.br>
2024-09-25 11:36:22 -0300

    a

--- mains.js
+++ mains.js
@@ -1,4 +1,10 @@
 function soma (numero1,numero2) {
     return numero1 + numero2
 }
-console.log (soma ( 3, 4 ) )
+console.log (soma ( 2, 3 ) )
+
+function Quadrado(numero1) {
+     return numero1*numero1
+}
+
+console.log(Quadrado (2) )
