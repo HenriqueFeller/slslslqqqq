88b598f623c874bd7f6f662b701a67455fbcca63
HenriqueFeller <henrique.feller@escola.pr.gov.br>
2024-09-25 12:28:18 -0300

    a

--- mains.js
+++ mains.js
@@ -1,16 +1,33 @@
-function soma(num1,num2){
-    return num1+num2;
+let CalculodeMediaDoUsuario = function Mediazinha(num1, num2, num3) {
+    num1 = parseInt(prompt("Me dê o primeiro número"))
+    num2 = parseInt(prompt("Me dê o segundo número"))
+    num3 = parseInt(prompt("Me dê o terceiro número"))
+    return (num1+num2+num3)/3   
 }
-console.log (soma (5,3));
+console.log(CalculodeMediaDoUsuario())
 
-function Quadrado (num1) {
+
+let Soma = function somaBasica(num1, num2) {
+    return num1+num2
+}
+
+console.log(Soma(2, 3))
+
+let Quadrado = function areaQuadrado(num1) {
     return num1*num1
 }
-console.log(Quadrado(2));
-let mediaa = function (n1, n2 , n3) {
 
-    return (n1 + n2 + n3) / 2;
- }
- console.log(media(6, 4, 2));
- let numero=prompt ((coloque numeros))
- document.write ( 6 )
+console.log(Quadrado(2))
+
+function Triangulo(num1, num2, num3) {
+    num3 = 2
+    return (num1*num2)/num3
+}
+
+console.log(Triangulo(2,3))
+
+let Media = function calculoMedia(num1, num2, num3) {
+    return (num1+num2+num3)/3
+}
+
+console.log(Media(2, 3, 10))
